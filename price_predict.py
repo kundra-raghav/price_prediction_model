72b41e1c028b317c67887c63d59d0bf90ce9d645
Raghav Kundra <62836073+kundra-raghav@users.noreply.github.com>
2023-09-23 18:23:39 +0530

    Add files via upload

---
+++ price_predict.py
@@ -0,0 +1,26 @@
+'''import pandas
+import matplotlib.pyplot as plot
+from sklearn.linear_model import LinearRegression
+
+
+data = pandas.read_csv('iphone_price.csv')
+plot.scatter(data['version'] , data ['price'])
+plot.show()
+
+model = LinearRegression()
+
+model.fit(data[['version']] , data[['price']])
+print(model.predict([[15]])) '''
+
+'''***from statistics import LinearRegression***'''
+import pandas as pd
+import matplotlib.pyplot as pt
+from sklearn.linear_model import LinearRegression as lr 
+
+data = pd.read_csv('housingdata.csv')
+pt.scatter(data['version'] , data ['price'])
+pt.show()
+
+model = LinearRegression()
+model.fit(data[['version']] , data[['price']])
+print(model.predict([['15']]))
